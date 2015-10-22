32bbfa4ae345a059a76c38b87de3d86072651a79
Gui Egas <guiegas@gmail.com>
2015-10-22 12:46:24 -0200

    Criando a Classe CBIR

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,9 +2,15 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Event\Event;
 
 class SiteController extends AppController
 {
+	public function beforeFilter(Event $event)
+	{
+		parent::beforeFilter($event);
+		$this->loadComponent('CBIR');
+	}
 	public function index()
     {
 
